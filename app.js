bff6a7468bebbda4477f0f0ea880acf5e33a037b
W1nde <markshadpalov@gmail.com>
2022-04-19 19:23:49 +0700

    main

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+}); 
+
 app.use('/users', require('./routes/user'));
 app.use('/cards', require('./routes/card'));
 
