fb918b0d41ee823fe21a4bf2e84a4f14e0d934c4
W1nde <markshadpalov@gmail.com>
2022-04-21 19:00:41 +0700

    hardcode

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 
 const app = express();
-const { PORT = 3000 } = process.env;
+// const { PORT = 3000 } = process.env;
 
 const bodyParser = require("body-parser");
 
