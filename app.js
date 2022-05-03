4d875a305653d9919513666ee6ff3bca0c29ef82
W1nde <markshadpalov@gmail.com>
2022-05-03 17:05:29 +0700

    main

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
-const express = require("express");
-const { default: mongoose } = require("mongoose");
+const express = require('express');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
-const { PORT = 3000 } = process.env;
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb://localhost:27017/mestodb", {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+const { login, createUser } = require('./controllers/user');
+const auth = require('./middlewares/auth');
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+app.use(auth);
+
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133" // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '625850df1e722638355859a0',
   };
 
   next();
 });
 
-app.use("/users", require("./routes/user"));
-app.use("/cards", require("./routes/card"));
+app.use('/cards', require('./routes/card'));
+app.use('/users', require('./routes/user'));
 
-app.all("*", (req, res) => {
-  res.status(404).send({ message: "Путь не найден" });
+const { PORT = 3000 } = process.env;
+
+app.all('*', (req, res) => {
+  res.status(404).send({ message: 'Путь не найден' });
 });
 
-app.listen(PORT);
+app.listen(PORT)
