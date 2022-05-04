96b619f27be1d1d1769c37102dfdc9bca6207d54
W1nde <markshadpalov@gmail.com>
2022-05-04 20:10:18 +0700

    main

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const auth = require('./middlewares/auth');
 
 app.post('/signin', login);
 app.post('/signup', createUser);
+
 app.use(auth);
 
 app.use((req, res, next) => {
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,43 @@
 const router = require("express").Router();
+const { celebrate, Joi } = require('celebrate');
 
-const {
-  getUser, getUserId, createUser, updateUserInfo, updateAvatar
-} = require("../controllers/user");
+const { getUser, getUserId, updateUserInfo, updateAvatar } = require("../controllers/user");
 
-router.get("/", getUser);
-router.get("/:userId", getUserId);
-router.post("/", createUser);
-router.patch("/me", updateUserInfo);
-router.patch("/me/avatar", updateAvatar);
+router.get('/', getUsers);
 
-module.exports = router;
+router.get('/me', getUser);
+
+router.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().length(24).hex().required(),
+    }),
+  }),
+  getUserId,
+);
+
+router.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string()
+        .pattern(
+          /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/,
+        )
+        .required(),
+    }),
+  }),
+  updateAvatar,
+);
+
+router.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUserInfo,
+);
